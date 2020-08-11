d504d541e7a0ebb7a6971d551854f4580bd133bc
Marcin Łaska <Marcin.laska@linux.pl>
2020-08-11 14:40:14 +0200

    Utworzenie i wyświetlenie formularza

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,12 +1,27 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\DiscountCodeGenerationOptions;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     public function home()
     {
-        return new Response('<!DOCTYPE html><title>Kody rabatowe</title><p>Generator kodów</p>');
+        $options = new DiscountCodeGenerationOptions;
+        $form    = $this->createFormBuilder($options)
+            ->add('codeCount',   NumberType::class, ['label' => 'Ile kodów?', 'html5' => true])
+            ->add('codeLength',  NumberType::class, ['label' => 'Jaka długość?', 'html5' => true])
+            ->add('codeContent', ChoiceType::class, ['label' => 'Z literami czy bez?', 'choices' => ['Tylko cyfry' => 'numeric', 'Cyfry i litery' => 'alphanumeric']])
+            ->add('generate',    SubmitType::class, ['label' => 'Generuj!'])
+            ->getForm();
+        
+        return $this->render('home.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Entity/DiscountCodeGenerationOptions.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Entity;
+
+class DiscountCodeGenerationOptions
+{
+    protected $codeCount;
+    protected $codeLength;
+    protected $codeContent;
+    
+    public function getCodeCount()
+    {
+        return $this->codeCount;
+    }
+    
+    public function setCodeCount($codeCount)
+    {
+        $this->codeCount = $codeCount;
+    }
+    
+    public function getCodeLength()
+    {
+        return $this->codeLength;
+    }
+    
+    public function setCodeLength($codeLength)
+    {
+        $this->codeCount = $codeLength;
+    }
+    
+    public function getCodeContent()
+    {
+        return $this->codeContent;
+    }
+    
+    public function setCodeContent($codeContent)
+    {
+        $this->codeContent = $codeContent;
+    }
+}
