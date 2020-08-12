4286da65bea05e619ac897bc39d3a3213a32e2b0
Marcin Łaska <Marcin.laska@linux.pl>
2020-08-12 11:58:46 +0200

    Pierwsze uruchomienie generatora poprzez komendę

--- src/Command/MakeDiscountCodesCommand.php
+++ src/Command/MakeDiscountCodesCommand.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Command;
 
+use App\Entity\DiscountCodeGenerationOptions;
+use App\Service\DiscountCodeGenerator;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -8,9 +11,22 @@ use Symfony\Component\Console\Output\OutputInterface;
 class MakeDiscountCodesCommand extends Command
 {
     protected static $defaultName = 'make:discount-codes';
+    protected $generator;
+    
+    public function __construct(DiscountCodeGenerator $generator)
+    {
+        $this->generator = $generator;
+        
+        parent::__construct();
+    }
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Witaj, świecie!');
+        $options = new DiscountCodeGenerationOptions;
+        $options->setCodeCount(3);
+        $options->setCodeLength(4);
+        $options->setCodeContent('numeric');
+        
+        $output->writeln($this->generator->setup($options)->run());
     }
 }
