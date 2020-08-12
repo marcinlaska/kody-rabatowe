5ec5eb166ccf47bb27400ac52a161fed5616ce11
Marcin Łaska <Marcin.laska@linux.pl>
2020-08-12 12:29:07 +0200

    Komenda obsługuje już opcje generowania kodów

--- src/Command/MakeDiscountCodesCommand.php
+++ src/Command/MakeDiscountCodesCommand.php
@@ -5,6 +5,7 @@ use App\Entity\DiscountCodeGenerationOptions;
 use App\Service\DiscountCodeGenerator;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -24,15 +25,19 @@ class MakeDiscountCodesCommand extends Command
     {
         $this
             ->setDescription('Generuje kody rabatowe')
-            ->setHelp('Generuje kody rabatowe do pliku na serwerze.');
+            ->setHelp('Generuje kody rabatowe do pliku na serwerze.')
+            
+            ->addArgument('count',   InputArgument::REQUIRED, 'Ilość kodów do wygenerowania.')
+            ->addArgument('length',  InputArgument::REQUIRED, 'Ilość znaków w każdym kodzie.')
+            ->addArgument('content', InputArgument::REQUIRED, 'Czy kody mogą zawierać litery (wartość "alphanumeric") czy tylko cyfry ("numeric").');
     }
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $options = new DiscountCodeGenerationOptions;
-        $options->setCodeCount(3);
-        $options->setCodeLength(4);
-        $options->setCodeContent('numeric');
+        $options->setCodeCount($input->getArgument('count'));
+        $options->setCodeLength($input->getArgument('length'));
+        $options->setCodeContent($input->getArgument('content'));
         
         $output->writeln($this->generator->setup($options)->run());
     }
